9d21d933dde2b8fb2d4fac1354dd04f012c70425
Aakaive <wonseok031@gmail.com>
2024-09-12 08:01:06 +0900

    Level 1-1. Early Return

--- src/main/java/org/example/expert/ExpertApplication.java
+++ src/main/java/org/example/expert/ExpertApplication.java
@@ -3,6 +3,7 @@ package org.example.expert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.context.ApplicationContext;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
